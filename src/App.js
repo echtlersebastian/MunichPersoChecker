9190c40720e1f04b27bc5a28e9a0787be0f137b3
Basti <mail@echtlersebastian.de>
2021-08-25 22:13:04 +0200

    changed Layout

--- src/App.js
+++ src/App.js
@@ -1,40 +1,22 @@
 import React, { useState  } from "react";
 import './App.css';
 import axios from "axios";
-import CookieConsent, { Cookies } from "react-cookie-consent";
+import validator from 'validator'
+import CookieConsent from "react-cookie-consent";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import '@fontsource/roboto';
+import Subscribe from "./Subscribe";
 
 function App() {
-  const [email, setEmail] = useState("");
-  const [nummer, setNummer] = useState("");
-  const toastError = () => toast.error("Your Ausweisnummer is already in use! Please select another one!");
-  const toastSuccess = () => toast.success("You successfully enabled the E-Mail Notification! Happy waiting!");
 
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-      const newContact = {
-        email: email,
-        number: nummer
-      }
 
-    axios.post('https://eu-central-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/persocheckerrealm-vyubq/service/AddContact/incoming_webhook/AddContact', newContact)
-        .then(response => {
-          if(response.status === 200){
-            toastSuccess();
-          }
-          }).catch(err => {
-            toastError();
-          });
-          setEmail("");
-          setNummer("");
-  }
-  return (
-    <div className="App">
+    return (
+      <div className="App">
       <ToastContainer/>
       <CookieConsent
   location="bottom"
-  buttonText="Nothing happens on this button."
+  buttonText="Alright, i got it!"
   cookieName="myAwesomeCookieName2"
   style={{ background: "#2B373B" }}
   buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
@@ -45,30 +27,21 @@ function App() {
       <header className="App-header">
         
         <p>
-          Welcome to the awesome Peronalausweis status checker!
-        </p>
-        <p>
-          This tool is only working, if your Personalausweis was issued by one of the Bügerbüros in Munich.
-        </p>
-        <p>
-
+        🎉🚀😍Welcome to the awesome Perso-Checker😍🚀🎉!
+    </p><Subscribe></Subscribe>
+          <h6>
+          🚀If you want to know what this tool does, check out the <a href="https://github.com/echtlersebastian/MunichPersoChecker">Repo</a> at Github!🚀
+            <br/>
+            <br/>
+            This tool is only working, if your Personalausweis was issued by one of the Bügerbüros in Munich.
+              <br/>
           Enter your Mailadress and your Personalausweisnummer here, and you will get notified, as soon, as the status of your Personalausweis changes.
-        </p>
-        <form onSubmit={handleSubmit}>
-          <label>
-            E-Mail:
-            <input type="text" name="email" value={email} onChange={e=>setEmail(e.target.value)} /> 
-          </label>
-          <label>
-            Personalausweisnummer:
-            <input type="text" name="name" value={nummer} onChange={e=>setNummer(e.target.value)} /> 
-            </label>  
-                
-          <input type="submit" value="Submit" />
-        </form>
+            </h6>
+      
+      
+   
    
       </header>
-   
     </div>
   );
 }
---
+++ src/Subscribe.js
@@ -0,0 +1,132 @@
+import React, { useState  } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { ToastContainer, toast } from 'react-toastify';
+import axios from "axios";
+import validator from 'validator'
+
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  text: {
+    color: "white"
+  },
+}));
+
+
+
+export default function Subscribe() {
+  const classes = useStyles();
+  const [email, setEmail] = useState("");
+  const [nummer, setNummer] = useState("");
+  const toastError = () => toast.error("Your Ausweisnummer is already in use! Please select another one!");
+  const toastNoMail = () => toast.error("Please Enter a valid E-Mail!");
+  const toastSuccess = () => toast.success("You successfully enabled the E-Mail Notification! Happy waiting!");
+  
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    if(validator.isEmail(email)){
+      const newContact = {
+        email: email,
+        number: nummer
+      }
+      
+      axios.post('https://eu-central-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/persocheckerrealm-vyubq/service/AddContact/incoming_webhook/AddContact', newContact)
+      .then(response => {
+        if(response.status === 200){
+          toastSuccess();
+        }
+      }).catch(err => {
+        toastError();
+      });
+      setEmail("");
+      setNummer("");
+    }else {
+      
+      toastNoMail();
+    }
+  }
+  
+
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <AccessAlarmIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Subscribe here
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={handleSubmit}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            nputProps={{
+                className: classes.input
+              }}
+            onChange={e=>setEmail(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="Ausweisnummer"
+            label="Ausweisnummer"
+            type="Ausweisnummer"
+            id="Ausweisnummer"
+            onChange={e=>setNummer(e.target.value)}
+          />
+        
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Subscribe
+          </Button>
+         
+        </form>
+      </div>
+     
+    </Container>
+  );
+}
